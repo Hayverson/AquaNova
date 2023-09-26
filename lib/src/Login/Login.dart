4d83720b605d1a287f8c6b7b4fb94ba1facbec98
Hayverson Enrique Chavarria Rojas <chavarriashayverson9@gmail.com>
2023-09-26 02:54:59 -0600

    main con problemas

---
+++ lib/src/Login/Login.dart
@@ -0,0 +1,64 @@
+/*import 'package:flutter/material.dart';
+
+void main() => runApp(const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Main(),
+    ));
+
+class Main extends StatelessWidget {
+  const Main({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Form(
+        autovalidateMode: AutovalidateMode.always,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 80.0, bottom: 80.0),
+                    child: Container(
+                      width: MediaQuery.sizeOf(context).width,
+                      height: 220,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: Image.asset(
+                            'assets/images/logoname.jpg',
+                          ).image,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const Padding(
+                padding: EdgeInsetsDirectional.only(top: 30.0, bottom: 20.0),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsetsDirectional.fromSTEB(15, 15, 15, 15),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.max,
+                          children: [],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+*/
