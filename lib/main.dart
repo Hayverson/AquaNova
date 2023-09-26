89e989865d643bcc39858cb59ca01390f00da931
Hayverson Enrique Chavarria Rojas <chavarriashayverson9@gmail.com>
2023-09-26 03:00:22 -0600

    se areclo el problema

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const Home(),
       debugShowCheckedModeBanner: false,
       title: 'Material App',
       initialRoute: '/',
